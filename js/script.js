72074fc9be922e4a7d825b79976a26cb1cd158ec
Mike-C25 <mirco.chiang@gmail.com>
2016-02-12 01:49:38 -0500

    changes v2
    - Created mini calendar on side along with "My Calendar" and "Other
    Calendars"
    - Fixed grid alignment issue
    - Fixed search button and create button
    - Fixed overall formatting

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-<script type="text/javascript">
+$(document).ready(function() {
+    
+    $('glyphicon-collapse-up').removeClass('glyphicon-collapse-up').addClass('glyphicon-triangle-right');
+    
+    $('glyphicon-collapse-down').removeClass('glyphicon-collapse-down').addClass('glyphicon-triangle-right');
+    
+    
+    $('#collapseCategory').on('show.bs.collapse', function() {
+        $('glyphicon-collapse-down').removeClass('glyphicon-collapse-down').addClass('glyphicon-triangle-bottom');
+    });
+    $('#collapseCategory').on('hide.bs.collapse', function() {
+        $('glyphicon-collapse-up').removeClass('glyphicon-collapse-up').addClass('glyphicon-triangle-right');
+    });
+});
 
-</script>
+/*
+$('#collapseCategory').on('show.bs.collapse', function(){
+$('a.accordion-toggle > i').removeClass('glyphicon-collapse-up').addClass('glyphicon-collapse-down');
+});  
+$('#collapseCategory').on('hide.bs.collapse', function(){
+$('a.accordion-toggle > i').removeClass('glyphicon-collapse-down').addClass('glyphicon-collapse-up');
+});   */  
